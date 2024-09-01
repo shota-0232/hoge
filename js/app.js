8e636f4f1e173c19be5397d0d57d650b4ae82c82
shota <shota.0232@gmail.com>
2024-09-01 09:36:56 +0900

    アップ漏れ

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(function () {
+
+  // フロートヘッダーメニュー
+  var targetHeight = $('.js-float-menu-target').height();
+  $(window).on('scroll', function() {
+    $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
+  });
+
+  // SPメニュー
+  $('.js-toggle-sp-menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+  });
+  $('.js-toggle-sp-menu-close').on('click', function () {
+    $(this).toggleClass('active');
+    $('.js-toggle-sp-menu-target').toggleClass('active');
+    $('.js-toggle-sp-menu').toggleClass('active');
+  });
+});
